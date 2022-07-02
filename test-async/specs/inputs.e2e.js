f04fa75af25e0af0e2098ddd0df3b64b94a866cd
James Mortensen <jmort253@gmail.com>
2022-07-02 23:41:01 +0530

    Fixed another case where await was inside the expect instead of outside

--- test-async/specs/inputs.e2e.js
+++ test-async/specs/inputs.e2e.js
@@ -14,22 +14,22 @@ describe('Test inputs page', () => {
 
     it('should show 34 when we enter 34', async () => {
         await browser.keys(['3', '4']);
-        expect(await inputElem).toHaveValue('34');
+        await expect(inputElem).toHaveValue('34');
     });
 
     it('should show 56 when we enter 5ty6', async () => {
         await browser.keys(['3', 't', 'y', '4']);
-        expect(await inputElem).toHaveValue('34');
+        await expect(inputElem).toHaveValue('34');
     });
 
     it('should allow decimals', async () => {
         await browser.keys(['3', '.', '4']);
-        expect(await inputElem).toHaveValue('3.4');
+        await expect(inputElem).toHaveValue('3.4');
     });
 
     it('should not allow any other special characters', async () => {
         await browser.keys(['~', '!', '@', '#', '$', '%', '^', '&', '-', '*', '*', '(', ')', '+', '=']);
-        expect(await inputElem).toHaveValue('');
+        await expect(inputElem).toHaveValue('');
         logger.log(await inputElem.getValue())
     })
 })
